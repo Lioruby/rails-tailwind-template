5cb92cc162ea5eab0aa8b45f000c22917d2786d2
Lior <71385272+Lioruby@users.noreply.github.com>
2021-03-29 19:00:05 +0200

    Little update

--- minimal_rspec_factory_bot.rb
+++ minimal_rspec_factory_bot.rb
@@ -131,7 +131,7 @@ after_bundle do
       end
       RSpec.configure do |config|
         # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-        config.fixture_path = "#{::Rails.root}/spec/fixtures"
+        # config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
         config.use_transactional_fixtures = true
 
